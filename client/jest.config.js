cc3dae78883ee2a4569f3e2d0cdefc3bef8edb54
jeffrmorton <jeffrey.raymond.morton@gmail.com>
2025-03-16 17:38:05 -0400

    Updated test to include client

--- client/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transformIgnorePatterns: [
-    '/node_modules/(?!axios|crypto-js|react-plotly.js|lodash.debounce|lodash.throttle|@mui|@emotion)'
-  ],
-  testEnvironment: 'jsdom',
-};
